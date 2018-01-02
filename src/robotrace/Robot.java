887c304f844d2dcddd4601ae21d5738f3507c6b3
illeke <ilse.smits.09.10@gmail.com>
2018-01-02 18:51:03 +0100

    robot

--- src/robotrace/Robot.java
+++ src/robotrace/Robot.java
@@ -37,10 +37,11 @@ class Robot {
      */
     public void draw(GL2 gl, GLU glu, GLUT glut, float tAnim) {
         gl.glPushMatrix();
+        //body and head
         gl.glColor3d(1,0.5,0.5);
         gl.glTranslated(0,0,1.35);
         glut.glutSolidSphere(0.45, 45, 45);
-        gl.glColor3d(0,0,0);
+        gl.glColor3d(1,1,1);
         gl.glTranslated(0.20,0.30,0);
         glut.glutSolidSphere(0.18, 18, 18);
         gl.glTranslated(-0.20,-0.30,0);
@@ -49,9 +50,38 @@ class Robot {
         gl.glTranslated(0.20,-0.30,-0.75);
         gl.glColor3d(0.5,0.5,0.8);
         glut.glutSolidCylinder(0.15, 0.40, 15, 40);
-        
-        
+        glut.glutSolidSphere(0.15, 15, 15);
+        gl.glTranslated(0,0,0.75);
+        //left leg
+        drawLeftLeg(gl,glu,glut,tAnim);
+        //right leg
+        drawRightLeg(gl,glu,glut,tAnim);
+        //left arm
+        drawLeftArm(gl,glu,glut,tAnim);
+        //right arm
+        drawRightArm(gl,glu,glut,tAnim);
         gl.glPopMatrix();
     }
-    
+ 
+
+    public void drawLeftLeg(GL2 gl, GLU glu, GLUT glut, float tAnim){
+        gl.glTranslated(1.5,0,-0.50);
+        gl.glRotated(90, 0, 0, 0);
+        gl.glColor3d(0.5,0.5,0.5);
+        glut.glutSolidCylinder(0.05, 0.30, 5, 15);
+ 
+        gl.glRotated(0, 0, 0, 0);
+    }
+
+    public void drawRightLeg(GL2 gl, GLU glu, GLUT glut, float tAnim){
+
+    }
+
+    public void drawLeftArm(GL2 gl, GLU glu, GLUT glut, float tAnim){
+
+    }
+
+    public void drawRightArm(GL2 gl, GLU glu, GLUT glut, float tAnim){
+
+    }
 }
