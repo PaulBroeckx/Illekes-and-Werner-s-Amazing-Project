81971aaa2d174394897691d3623b219d75620d08
Smits <i.m.smits@student.tue.nl>
2018-01-08 11:18:33 +0100

    shaders

--- build/classes/robotrace/shaderPrograms/Terrain/vertex.glsl
+++ build/classes/robotrace/shaderPrograms/Terrain/vertex.glsl
@@ -1,7 +1,18 @@
+#version 120
 // simple vertex shader
 
-void main()
-{
-    gl_Position    = gl_ModelViewProjectionMatrix * gl_Vertex;      // model view transform
-    gl_FrontColor = gl_Color;
+float offset(vec3 p) {
+	vec3 v = normalize(p);
+        v.z = 0.6 * cos(0.3*v.x + 0.2*v.y) + 0.4 * cos(v.x - 0.5*v.y);
+        return v; 
+}
+
+void main() {
+	// convert from homogeneous coordinates to 3D-coordinates
+	vec3 p = gl_Vertex.xyz/gl_Vertex.w;
+
+	// output of vertex shader
+	gl_FrontColor = gl_Color;
+	gl_TexCoord[0] = gl_MultiTexCoord0;
+	gl_Position = gl_ModelViewProjectionMatrix * vec4(p, 1);
 }
