e5eadd4db26c9b21a99a3676917b97b142a4ee9b
Broeckx <p.j.a.m.broeckx@student.tue.nl>
2018-01-08 11:15:19 +0100

    no build

--- build/classes/robotrace/shaderPrograms/Track/vertex.glsl
+++
@@ -1,9 +0,0 @@
-// simple vertex shader
-uniform sampler2D tex; 
-
-void main()
-{
-    gl_Position    = gl_ModelViewProjectionMatrix * gl_Vertex;      // model view transform
-    gl_FrontColor = gl_Color;
-    gl_TexCoord[0] = gl_MultiTexCoord0;
-}
